9d0e59951ced0da13b1ff6ec5ed7818c14b41504
Vasiliki Trachani <trachani.vasiliki@gmail.com>
2021-12-08 22:57:07 +0200

    Initial commit

---
+++ SnowFallEffect/SnowFallEffect.h
@@ -0,0 +1,18 @@
+//
+//  SnowFallEffect.h
+//  SnowFallEffect
+//
+//  Created by Vasiliki Trachani on 8/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SnowFallEffect.
+FOUNDATION_EXPORT double SnowFallEffectVersionNumber;
+
+//! Project version string for SnowFallEffect.
+FOUNDATION_EXPORT const unsigned char SnowFallEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowFallEffect/PublicHeader.h>
+
+
