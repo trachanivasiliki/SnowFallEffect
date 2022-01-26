f833e692ee86bec13468435e76f2493bc1ba4826
Vasiliki Trachani <trachani.vasiliki@gmail.com>
2022-01-26 19:38:29 +0200

    tests

--- SnowFallEffectTests/SnowFallEffectTests.swift
+++ SnowFallEffectTests/SnowFallEffectTests.swift
@@ -13,36 +13,35 @@ class SnowFallEffectTests: XCTestCase {
     var snowFallEffect: SnowFallEffectLayer!
     
     override func setUp() {
-        snowFallEffect = SnowFallEffectLayer()
+        snowFallEffect = SnowFallEffectLayer(snowFallItems: [])
     }
 
     func testSnowFallEffectItem() throws {
-        let fallingItem = SnowFallEffectItem(particleImage: UIImage(), speed: 20, scale: 1, color: .white)
+        let fallingItem = SnowFallEffectItem(particleImage: UIImage(), speed: 20, scale: 1, velocity: 10)
         
-        XCTAssertEqual(fallingItem.color, .white)
         XCTAssertEqual(fallingItem.image, UIImage())
         XCTAssertEqual(fallingItem.speed, 20)
         XCTAssertEqual(fallingItem.scale, 1)
+        XCTAssertEqual(fallingItem.velocity, 10)
     }
 
     func testSnowFallEffectItem_GivenDefaultValued() throws {
-        let fallingItem = SnowFallEffectItem()
+        let fallingItem = SnowFallEffectItem(velocity: 5)
         
-        XCTAssertEqual(fallingItem.color, .white)
         XCTAssertEqual(fallingItem.image, UIImage())
         XCTAssertEqual(fallingItem.speed, 1)
         XCTAssertEqual(fallingItem.scale, 1)
+        XCTAssertEqual(fallingItem.velocity, 5)
     }
     
     func testConfigureSnowFallLayer() throws {
-        let emitterCells = snowFallEffect.configure(with: [SnowFallEffectItem(particleImage: UIImage(), speed: 20, scale: 1, color: .white)])!
+        let emitterCells = snowFallEffect.configure(with: [SnowFallEffectItem(particleImage: UIImage(), speed: 20, scale: 1, velocity: 10)])!
         XCTAssertEqual(emitterCells.count, 1)
     }
     
     func testCongifureSnowLayer_GivenDefaultValues() throws {
-        let _ = snowFallEffect.configure(with: [SnowFallEffectItem()])!
-        XCTAssertEqual(snowFallEffect.snowFallDuration, SnowFallEffectDuration.infinitive)
+        let _ = snowFallEffect.configure(with: [SnowFallEffectItem(velocity: 5)])!
+        XCTAssertEqual(snowFallEffect.snowFallDuration, SnowFallEffectDuration.infinite)
         XCTAssertEqual(snowFallEffect.snowFallDirection, SnowFallEffectDirection.top)
-        XCTAssertEqual(snowFallEffect.snowFallShape, SnowFallShape.line)
     }
 }
